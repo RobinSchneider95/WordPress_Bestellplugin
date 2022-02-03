f7b811dbea8e5fa707622c079342352d500fe932
Robin Schneider <46456719+RobinSchneider95@users.noreply.github.com>
2022-02-03 17:33:19 +0000

    Erste html und php Datei erstellt

---
+++ easyfoodstore.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Easy Food Store
+Beschreibung: Ein Plugin zur Einbindung einer Bestelloberfläche für fertige Gerichte
+Version: 0.1
+Autor: Robin Schneider
+*/
+
+defined('ASPATH') or die ('Are you ok?' );
+?>
