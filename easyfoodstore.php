c192fc71a4fb0b4e0db74323807614c45615e3aa
Robin Schneider <46456719+RobinSchneider95@users.noreply.github.com>
2022-02-04 17:32:14 +0100

    Add files via upload
    Erste Funktionen aus einem Tutorial

--- easyfoodstore.php
+++ easyfoodstore.php
@@ -1,27 +1,45 @@
 <?php
 /*
 Plugin Name: Easy Food Store
-Beschreibung: Ein Plugin zur Einbindung einer Bestelloberfläche für fertige Gerichte
-Version: 0.1
+description: Der Easy Food Store bietet einem die Möglichkeit über die eigene Webseite eine Interaktive Speisekarte einzubinden. Sie bietet ein Bestellformular für Kunden, mit Login zur Beschleunigung des Bestellvorganges durch speichern von Favoriten, Lieferadressen und Bezahloptionen. Die Verwaltung bietet die Möglichkeit Bestellungen einzusehen, sowie Speisekarte und Bezahloptionen zu verwalten.
+Version: DEV-0.2
 Autor: Robin Schneider
 */
+//Erstmal ein Testcode zum üben
 
-defined('ASPATH') or die ('Are you ok?' );
+//Grundgerüst des Plugins, ohne Bereichsbezogene Funktionen
+//ABSPATH muss in der wp-config.php gesetzt sein - Sicherheit, Datei kann nicht auserhalb einer WordPress Instanz ausgefürt werden
+defined( 'ABSPATH' ) or die ( 'Are you ok?' );
 
-$start_store = new StoreOrder();
+$start_reminder = new UpdateReminder();
 
-class StoreOrder{
-  public function __construct() {
-  
-  }  
+class UpdateReminder{
+	public function __construct(){
+		register_activation_hook( __FILE__, array( $this, 'updateReminder_activate' ));
+		register_deactivation_hook( __FILE__, array( $this, 'updateReminder_deactivation' ));
+		add_action( 'updateReminderEvent', array( $this, 'getUpdateStatus' ));
+	}
 }
 
-register_activation_hook(__FILE__, array($this, 'StoreOrder_activate'));
-
-public function StoreOrder_activate(){
-  if (!wp_next_scheduled('StoreOrderEvent')){
-      wp_schedule_event(time(), 'daily', 'StoreOrderEvent');
-  }
+public function getUpdateStatus(){
+	$status = wp_get_update_date();
 }
+
+//WordPress Cron Event Aktivieren
+public function updateReminder_activate(){
+	if (! wp_next_scheduled ( 'updateReminderEvent' ) ) {
+		wp_schedule_event(time(), 'daily', 'updateReminderEvent');
+	}
+}
+
+//WordPress Cron Event Deaktivieren
+public function updateReminder_deactivation(){
+	if( wp_next_scheduled( 'updateReminderEvent' )){
+		wp_clear_scheduled_hook( 'updateReminderEvent' );
+	}
+}
+//Funktionsgerüst des Plugins
+
+
 
 ?>
