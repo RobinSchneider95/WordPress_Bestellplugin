4c875847de6a5c1c0b502647208162fac8ee512e
Robin Schneider <46456719+RobinSchneider95@users.noreply.github.com>
2022-02-04 11:03:02 +0000

    erste Grundlagen fürs Plugin

--- easyfoodstore.php
+++ easyfoodstore.php
@@ -7,4 +7,21 @@ Autor: Robin Schneider
 */
 
 defined('ASPATH') or die ('Are you ok?' );
+
+$start_store = new StoreOrder();
+
+class StoreOrder{
+  public function __construct() {
+  
+  }  
+}
+
+register_activation_hook(__FILE__, array($this, 'StoreOrder_activate'));
+
+public function StoreOrder_activate(){
+  if (!wp_next_scheduled('StoreOrderEvent')){
+      wp_schedule_event(time(), 'daily', 'StoreOrderEvent');
+  }
+}
+
 ?>
