67eff339a4a1a5e3c7076a62b3b8de877b537ed0
Robin Schneider <46456719+RobinSchneider95@users.noreply.github.com>
2022-03-22 16:03:11 +0100

    Update easyfoodstore.php
    Erste offizielle Development Version

--- easyfoodstore.php
+++ easyfoodstore.php
@@ -1,27 +1,111 @@
 <?php
-/*
-Plugin Name: Easy Food Store
-Beschreibung: Ein Plugin zur Einbindung einer Bestelloberfläche für fertige Gerichte
-Version: 0.1
-Autor: Robin Schneider
-*/
+	defined('ABSPATH') or die("Thanks");
+	/*
+	* Plugin Name:  EasyFoodStore
+	* Description: Ein Plugin zur Integration eines Bestellformular und Lieferdienst
+	* Version: 0.1.2
+	* Author: Robin Schneider
+	* License: GPL2
+	* License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+	* Text Domain:  EasyFoodStore
+	*/
+    //------------------------------------------------------------------
+    //Backend Menü Optionen
+	function easyfoodstore_admin_menu(){
+		add_menu_page('EasyFoodStore', 'EasyFoodStore', 'manage_options', 'easyfoodstore_menu', 'easyfoodstore_admin_menu_site', 'dashicons-food', 20);
+	}
 
-defined('ASPATH') or die ('Are you ok?' );
+	function easyfoodstore_admin_submenus(){
+		add_submenu_page("easyfoodstore_menu", "Einstellungen", "Einstellungen", "manage_options", "easyfoodstore_menu", "easyfoodstore_admin_menu_site");
+		add_submenu_page("easyfoodstore_menu", "Gerichtkategorien", "Gerichtkategorien", "manage_options", "category_menu", "category_admin_submenu_site");
+		add_submenu_page("easyfoodstore_menu", "Gerichte", "Gerichte", "manage_options", "gerichte_menu", "gerichte_admin_submenu_site");
+		add_submenu_page("easyfoodstore_menu", "Bestellungen", "Bestellungen", "manage_options", "bestellungen_menu", "bestellungen_admin_submenu_site");
+	}
 
-$start_store = new StoreOrder();
+	add_action("admin_menu", "easyfoodstore_admin_menu");
+	add_action('admin_menu', 'easyfoodstore_admin_submenus');
+	//------------------------------------------------------------------
+	//Benutzeroberfläche im Backend
+	//Einstellungen
+	function easyfoodstore_admin_menu_site(){
+		?>
+		<h1>Einstellungen</h1>
+		<form action="options.php" method="post">
+		<?php
+			settings_fields("general_option_group");
+			do_settings_sections("general_menu");
+			submit_button("Änderungen Speichern");
+		?>
+		</form>
+		<?php
+    }
+	//------------------------------------------------------------------
+	//Gericht Kategorien
+	function category_admin_submenu_site(){
+		?>
+		<h1>Gerichtkategorien</h1>
+		<form action="options.php" method="post">
+		<?php
+		settings_fields("category_option_group");
+		do_settings_sections("category_menu");
+		submit_button("Änderungen Speichern");
+		?>
+		</form>
+		<?php
+	}
 
-class StoreOrder{
-  public function __construct() {
-  
-  }  
-}
+	//Fügt Benutzeroberfläche im Backend hinzu
+	function category_admin_init(){
+		register_setting("category_option_group", "first_option");
+		add_settings_section("category_section", "Name der Kategorie", "category_menu");
+		add_settings_field("first_option_field", "Name", "category_first_option_markup", "category_menu", "category_section");
+	}
 
-register_activation_hook(__FILE__, array($this, 'StoreOrder_activate'));
+	add_action("categorie_admin", "category_admin_init");
+	
+	function counter_first_option_markup(){
+		?>
+			<input type="text" id="first_option" name="first_option" value="<?php echo get_option("first_option"); ?>">
+		<?php
+	}
 
-public function StoreOrder_activate(){
-  if (!wp_next_scheduled('StoreOrderEvent')){
-      wp_schedule_event(time(), 'daily', 'StoreOrderEvent');
-  }
-}
+	//------------------------------------------------------------------
+	//Gerichte
+	function gerichte_admin_submenu_site(){
+		?>
+		<h1>Gerichte</h1>
+		<form action="options.php" method="post">
+		<?php
+		settings_fields("dish_option_group");
+		do_settings_sections("dish_menu");
+		submit_button("Änderungen Speichern");
+		?>
+		</form>
+		<?php
+	}
+	//------------------------------------------------------------------
+	//Bestellungen
+	function bestellungen_admin_submenu_site(){
+		?>
+		<h1>Bestellungen</h1>
+		<form action="options.php" method="post">
+		<?php
+		
 
+		?>
+		</form>
+		<?php
+	}
+
+	
+
+
+	//------------------------------------------------------------------
+	//Frontend
+
+	function storepage(){
+		include('storepage.php');
+	}
+
+	add_shortcode("easyfoodstore_shortcode", "storepage");
 ?>
